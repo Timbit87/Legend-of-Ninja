3a4fb750384e0b4405f6d88cddd295cfc27c88c5
Andrew Beckett <149121935+Timbit87@users.noreply.github.com>
2025-05-02 11:47:22 +0900

    Saved enemy script

--- enemy.gd
+++ enemy.gd
@@ -1,1 +1,46 @@
 extends CharacterBody2D
+
+
+@export var HP: int = 4
+@export var damage: int = 1
+@export var speed: float = 30
+@export var acceleration: float = 5
+@export var death_particles: PackedScene
+@export var sounds: Array[AudioStream] = []
+
+var target: Node2D
+var is_dead = false
+
+func take_damage(amount: int = 1, attacker: Node2D = null):
+	if is_dead:
+		return
+	HP -= amount
+	if HP <= 0:
+		death()
+	var flash_red_colour: Color = Color(50, .1, .1)
+	var original_colour: Color = Color(1, 1, 1)
+	
+	for i in range(2):
+		modulate = flash_red_colour
+		await get_tree().create_timer(0.05).timeout	
+		modulate = original_colour
+		await get_tree().create_timer(0.05).timeout	
+
+func death():
+	is_dead = true
+	velocity = Vector2.ZERO
+	if death_particles:
+		var particles = death_particles.instantiate()
+		particles.global_position = global_position
+		get_tree().current_scene.add_child(particles)
+	queue_free()
+
+func chase_target():
+	if target and not is_dead:
+		var dir = (target.global_position - global_position).normalized()
+		velocity = velocity.move_toward(dir * speed, acceleration)
+		
+func _physics_process(delta):
+	if not is_dead:
+		chase_target()
+		move_and_slide()
