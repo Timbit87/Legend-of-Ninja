92749dc046f235b3e086f41abe0ce96e68921840
Andrew Beckett <149121935+Timbit87@users.noreply.github.com>
2025-10-09 11:14:47 +0900

    Main menu added

---
+++ Scenes/Main Menu/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/NewGameLabel.pressed.connect(_on_start_pressed)
+	$VBoxContainer/LoadGameLabel.pressed.connect(_on_load_pressed)
+	$VBoxContainer/QuitLabel.pressed.connect(_on_quit_pressed)
+	
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/GameScene/game_scene.tscn")
+	
+func _on_load_pressed():
+	print("Load game not implemented")
+	
+func _on_quit_pressed():
+	get_tree().quit
